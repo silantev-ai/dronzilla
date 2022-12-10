53b95ee7a96c142fb2e61e9ab1483710b4bba150
Silantev <lesha.vlg@gmail.com>
2022-12-10 21:44:13 +0300

    init

---
+++ src/main/java/alex/silantev/dronzilla/DronzillaApplication.java
@@ -0,0 +1,13 @@
+package alex.silantev.dronzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronzillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alex/silantev/dronzilla/DronzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.silantev.dronzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
